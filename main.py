f990c44257b59991bd9e92c549ac55f6dcd50f2a
Dimdd <108973368+Dimdd@users.noreply.github.com>
2022-07-27 12:13:30 +0300

    Initial Commit

---
+++ main.py
@@ -0,0 +1,10 @@
+min_number = int(input())
+max_number = int(input())
+for a in range(min_number, max_number+1):
+    for b in range(min_number, max_number+1):
+        for c in range(min_number, max_number+1):
+            for d in range(min_number, max_number+1):
+                if a % 2 == 0 and d % 2 != 0 or a % 2 != 0 and d % 2 == 0:
+                    if (b + c) % 2 == 0:
+                        if a>d:
+                            print(f"{a}{b}{c}{d}", end=" ")
